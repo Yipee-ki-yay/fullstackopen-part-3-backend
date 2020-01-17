7f5af807918a01713b51fa994d1559087821662a
yipee <yipee@macBookPro.local>
2020-01-17 22:25:17 +0200

    implement tasks from part_3 a

---
+++ index.js
@@ -0,0 +1,114 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+morgan.token('data', function (req, res) { return JSON.stringify({"name": req.body.name, "number": req.body.number })})
+
+app.use(bodyParser.json());
+// app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :data'))
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  const personsLength = persons.length;
+  const date = new Date();
+
+  res.send(
+    `<div>
+      <div>Phonebook has info for ${personsLength} people</div>
+      <div>${date}</div>
+    </div>`
+  )
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id);
+
+  if ( person ) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  res.status(204).end()
+})
+
+const generateId = () => Math.floor(Math.random() * 10000000)
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+  const isNameExist = persons.some(p => p.name === body.name);
+  console.log(isNameExist);
+  
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  if ( isNameExist ) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person)
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
